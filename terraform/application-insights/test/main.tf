a20f6b93188d4a334e9db06c41efe55920b37a8b
murggu <amurguzur@gmail.com>
2022-10-19 10:48:28 +0200

    Fix naming

--- terraform/application-insights/test/main.tf
+++ terraform/application-insights/test/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-adl-001-tf-state"
-    storage_account_name = "stadl001tfstate"
+    resource_group_name  = "rg-adl-terraform-state"
+    storage_account_name = "stadlterraformstate"
     container_name       = "default"
     key                  = "applicationinsights.terraform.tfstate"
   }
