a49267e8e95a491b2149ff49e49ce32100464720
Walter Grasselli <38637309+waltergrasselli@users.noreply.github.com>
2022-11-09 02:40:02 +0100

    Update outputs.tf

--- terraform/aks-cluster/test/outputs.tf
+++ terraform/aks-cluster/test/outputs.tf
@@ -1,1 +1,14 @@
 
+output "aks_id" {
+  value = module.adl_aks.id
+}
+
+output "aks_name" {
+  value = module.adl_aks.name
+}
+
+
+output "aks_all" {
+  value     = module.adl_aks
+  sensitive = true
+}
