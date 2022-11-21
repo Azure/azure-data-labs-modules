2bfe59821e8417fb52bb6320085020e354e719d8
murggu <amurguzur@gmail.com>
2022-11-21 15:58:37 +0100

    remove data dependencies

--- terraform/virtual-machine/test/virtual_machine.tf
+++ terraform/virtual-machine/test/virtual_machine.tf
@@ -5,7 +5,7 @@ module "virtual_machine" {
   rg_name  = var.rg_name
   location = var.location
 
-  subnet_id = data.azurerm_subnet.snet_default.id
+  subnet_id = module.local_snet_default.id
 
   jumphost_username = var.jumphost_username
   jumphost_password = var.jumphost_password
@@ -15,11 +15,23 @@ module "virtual_machine" {
   tags = {}
 }
 
-# Data dependencies
+# Module dependencies
 
-data "azurerm_subnet" "snet_default" {
-  name                 = local.snet_name
-  virtual_network_name = local.vnet_name
-  resource_group_name  = var.rg_name
+module "local_vnet" {
+  source = "../../virtual-network"
+
+  rg_name  = var.rg_name
+  basename = random_string.postfix.result
+  location = var.location
+
+  address_space = ["10.0.0.0/16"]
 }
 
+module "local_snet_default" {
+  source = "../../subnet"
+
+  rg_name          = var.rg_name
+  name             = "vnet-${random_string.postfix.result}-vm-default"
+  vnet_name        = module.local_vnet.name
+  address_prefixes = ["10.0.6.0/24"]
+}
--- terraform/virtual-network/variables.tf
+++ terraform/virtual-network/variables.tf
@@ -23,3 +23,15 @@ variable "address_space" {
   type        = list(string)
   description = "The address space that is used the virtual network"
 }
+
+variable "dns_servers" {
+  description = "List of dns servers to use for virtual network"
+  type        = list(string)
+  default     = []
+}
+
+variable "ddos_protection_plan" {
+  description = "A ddos_protection_plan id "
+  type        = map(string)
+  default     = {}
+}
