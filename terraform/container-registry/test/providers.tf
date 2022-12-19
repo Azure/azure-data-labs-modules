f298e018cc0a065b93196837a3d8f3af8e820ef0
Aitor Murguzur <amurguzur@gmail.com>
2022-12-19 14:18:20 +0100

    upgrade to 3.36.0 (#38)

--- terraform/container-registry/test/providers.tf
+++ terraform/container-registry/test/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-adl-terraform-state"
-    storage_account_name = "stadlterraformstate"
+    storage_account_name = "stadltfstate"
     container_name       = "default"
     key                  = "containerregistry.terraform.tfstate"
   }
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.32.0"
+      version = "= 3.36.0"
     }
   }
 
