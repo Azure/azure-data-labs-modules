a6fd6cf6961d7202bcd1298cb608d2ace33da035
murggu <amurguzur@gmail.com>
2022-12-23 16:37:36 +0100

    fix #21

---
+++ terraform/log-analytics/log-analytics-cluster/test/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  tags = {
+    Project = "Azure/azure-data-labs-modules"
+    Module  = "log-analytics-cluster"
+    Toolkit = "Terraform"
+  }
+}
---
+++ terraform/log-analytics/log-analytics-cluster/test/log_analytics_cluster.tf
@@ -0,0 +1,20 @@
+module "log_analytics_cluster" {
+  source = "../"
+
+  basename = random_string.postfix.result
+  rg_name  = module.local_rg.name
+  location = var.location
+
+  tags = {}
+}
+
+# Modules dependencies
+
+module "local_rg" {
+  source = "../../../resource-group"
+
+  basename = random_string.postfix.result
+  location = var.location
+
+  tags = local.tags
+}
