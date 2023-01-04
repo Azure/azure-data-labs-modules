694323e82641638ff7f6fe0c4fa1ef9b736326d6
murggu <amurguzur@gmail.com>
2023-01-04 12:40:28 +0100

    complete args

--- terraform/service-plan/main.tf
+++ terraform/service-plan/main.tf
@@ -4,8 +4,14 @@ resource "azurerm_service_plan" "adl_sp" {
   name                = "sp-${var.basename}"
   resource_group_name = var.rg_name
   location            = var.location
-  os_type             = var.os_type
-  sku_name            = var.sku_name
+
+  os_type                      = var.os_type
+  sku_name                     = var.sku_name
+  app_service_environment_id   = var.app_service_environment_id
+  maximum_elastic_worker_count = var.maximum_elastic_worker_count
+  worker_count                 = var.worker_count
+  per_site_scaling_enabled     = var.per_site_scaling_enabled
+  zone_balancing_enabled       = var.zone_balancing_enabled
 
   tags = var.tags
 }
