b5fb9d904e65e290769f86b81b6ccb13691eac8d
murggu <amurguzur@gmail.com>
2023-01-09 12:56:06 +0100

    add new vars

--- terraform/function-app/linux-function-app/main.tf
+++ terraform/function-app/linux-function-app/main.tf
@@ -1,15 +1,23 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_function_app
 
 resource "azurerm_linux_function_app" "adl_func_linux" {
-  name                = "func-${var.basename}"
-  resource_group_name = var.rg_name
-  location            = var.location
-
+  name                       = "func-${var.basename}"
+  resource_group_name        = var.rg_name
+  location                   = var.location
+  
+  service_plan_id            = var.service_plan_id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
-  service_plan_id            = var.service_plan_id
+  enabled                    = var.enabled
+  https_only                 = var.https_only
+  client_certificate_enabled = var.client_certificate_enabled
+  virtual_network_subnet_id  = var.virtual_network_subnet_id
 
   site_config {}
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = var.tags
 }
