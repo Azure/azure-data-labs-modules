76886bc945dcd5e19d8aad862c43822f043b1b7a
Walter Grasselli <38637309+waltergrasselli@users.noreply.github.com>
2023-01-18 21:08:34 -0300

    adding locals values

--- terraform/sql-managed-instance/test/locals.tf
+++ terraform/sql-managed-instance/test/locals.tf
@@ -1,5 +1,9 @@
 locals {
-  snet_name = "snet-adl-modules-test-default"
-  vnet_name = "vnet-adl-modules-test"
-  # dns_sql_server = "privatelink.database.windows.net" 
+  tags = {
+    Project = "Azure/azure-data-labs-modules"
+    Module  = "sql-managed-instance"
+    Toolkit = "Terraform"
+  }
+
+  dns_sql_server = "privatelink.database.windows.net"
 }
