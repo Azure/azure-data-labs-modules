203dd20dba4b264f0e7536821acec8c86fda9a3d
Ignacio Alonso Portillo <ignacio.alonso@microsoft.com>
2023-02-05 13:07:16 +0100

    Fix Resource Group validation condition.

--- terraform/sql-managed-instance/variables.tf
+++ terraform/sql-managed-instance/variables.tf
@@ -7,7 +7,7 @@ variable "rg_name" {
   type        = string
   description = "Resource group name."
   validation {
-    condition     = can(regex("^[-\\w\\.\\(\\)]{1,90}", var.rg_name)) && can(regex("[\\w]+$", var.rg_name))
+    condition     = can(regex("^[-\\w\\.\\(\\)]{1,90}$", var.rg_name)) && can(regex("[-\\w\\(\\)]+$", var.rg_name))
     error_message = "Resource group names must be between 1 and 90 characters and can only include alphanumeric, underscore, parentheses, hyphen, period (except at end)."
   }
 }
