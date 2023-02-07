fd095d0353d3778ab12c4ae3b3eff27d7facef3f
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-07 22:01:37 +0100

    Fix to proper resource type name.

--- terraform/aks-cluster/outputs.tf
+++ terraform/aks-cluster/outputs.tf
@@ -1,27 +1,27 @@
 output "id" {
   value = (
-    length(azurerm_analysis_services_server.adl_as) > 0 ?
+    length(azurerm_kubernetes_cluster.adl_as) > 0 ?
     azurerm_kubernetes_cluster.adl_aks[0].id : ""
   )
 }
 
 output "name" {
   value = (
-    length(azurerm_analysis_services_server.adl_as) > 0 ?
+    length(azurerm_kubernetes_cluster.adl_as) > 0 ?
     azurerm_kubernetes_cluster.adl_aks[0].name : ""
   )
 }
 
 output "resource_group_name" {
   value = (
-    length(azurerm_analysis_services_server.adl_as) > 0 ?
+    length(azurerm_kubernetes_cluster.adl_as) > 0 ?
     azurerm_kubernetes_cluster.adl_aks[0].resource_group_name : ""
   )
 }
 
 output "all" {
   value = (
-    length(azurerm_analysis_services_server.adl_as) > 0 ?
+    length(azurerm_kubernetes_cluster.adl_as) > 0 ?
     azurerm_kubernetes_cluster.adl_aks[0] : ""
   )
   sensitive = true
