d9f4a02f193ff3e6dcef52dce1d6bf18c467ef49
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-07 10:37:35 +0100

    Fix typo in output name.

--- terraform/event-hubs/event-hubs-namespace/test/outputs.tf
+++ terraform/event-hubs/event-hubs-namespace/test/outputs.tf
@@ -6,6 +6,6 @@ output "name" {
   value = module.event_hubs_namespace.name
 }
 
-output "resouce_group_name" {
-  value = module.event_hubs_namespace.resouce_group_name
+output "resource_group_name" {
+  value = module.event_hubs_namespace.resource_group_name
 }
