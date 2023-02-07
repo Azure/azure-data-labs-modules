ec349b307bf355636da955cb2acd913444667b91
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-07 13:28:51 +0100

    Fix event-hubs test outputs.

--- terraform/event-hubs/event-hubs/test/outputs.tf
+++ terraform/event-hubs/event-hubs/test/outputs.tf
@@ -1,21 +1,21 @@
 output "id" {
-  value = module.event_hubs_namespace.id
+  value = module.event_hubs.id
 }
 
 output "name" {
-  value = module.event_hubs_namespace.name
+  value = module.event_hubs.name
 }
 
 output "resource_group_name" {
-  value = module.event_hubs_namespace.resource_group_name
+  value = module.event_hubs.resource_group_name
 }
 
 output "primary_connection_string" {
-  value = module.event_hubs_namespace.primary_connection_string
+  value = module.event_hubs.primary_connection_string
   sensitive = true
 }
 
 output "secondary_connection_string" {
-  value = module.event_hubs_namespace.secondary_connection_string
+  value = module.event_hubs.secondary_connection_string
   sensitive = true
 }
