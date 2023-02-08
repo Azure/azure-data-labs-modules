8274e2a11a945c6f1dcb2d21c6c58dbe7dcb561f
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 12:25:33 +0100

    Make virtual-network module optional

--- terraform/virtual-network/outputs.tf
+++ terraform/virtual-network/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_virtual_network.adl_vnet.id
+  value = (
+    length(azurerm_virtual_network.adl_vnet) > 0 ?
+    azurerm_virtual_network.adl_vnet[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_virtual_network.adl_vnet.name
+  value = (
+    length(azurerm_virtual_network.adl_vnet) > 0 ?
+    azurerm_virtual_network.adl_vnet[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_virtual_network.adl_vnet.resource_group_name
+  value = (
+    length(azurerm_virtual_network.adl_vnet) > 0 ?
+    azurerm_virtual_network.adl_vnet[0].resource_group_name : ""
+  )
 }
