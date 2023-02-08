9dca56c6eb0015f101ee1bbc660632953914c87e
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 11:03:25 +0100

    Make key--vault  module optional

--- terraform/key-vault/outputs.tf
+++ terraform/key-vault/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_key_vault.adl_kv.id
+  value = (
+    length(azurerm_key_vault.adl_kv) > 0 ?
+    azurerm_key_vault.adl_kv[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_key_vault.adl_kv.name
+  value = (
+    length(azurerm_key_vault.adl_kv) > 0 ?
+    azurerm_key_vault.adl_kv[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_key_vault.adl_kv.resource_group_name
+  value = (
+    length(azurerm_key_vault.adl_kv) > 0 ?
+    azurerm_key_vault.adl_kv[0].resource_group_name : ""
+  )
 }
