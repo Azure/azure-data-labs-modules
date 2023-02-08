d9f21e097dd301e6c2f2f703ddf413691b419629
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 10:10:08 +0100

    TF FMT

--- terraform/cognitive-search/outputs.tf
+++ terraform/cognitive-search/outputs.tf
@@ -1,20 +1,20 @@
 output "id" {
-	value =(
-		length(azurerm_search_service.adl_srch) > 0 ?
-		azurerm_search_service.adl_srch[0].id : ""
-	)
+  value = (
+    length(azurerm_search_service.adl_srch) > 0 ?
+    azurerm_search_service.adl_srch[0].id : ""
+  )
 }
 
 output "name" {
-	value =(
-		length(azurerm_search_service.adl_srch) > 0 ?
-		azurerm_search_service.adl_srch[0].name : ""
-	)
+  value = (
+    length(azurerm_search_service.adl_srch) > 0 ?
+    azurerm_search_service.adl_srch[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-	value =(
-		length(azurerm_search_service.adl_srch) > 0 ?
-		azurerm_search_service.adl_srch[0].resource_group_name : ""
-	)
+  value = (
+    length(azurerm_search_service.adl_srch) > 0 ?
+    azurerm_search_service.adl_srch[0].resource_group_name : ""
+  )
 }
