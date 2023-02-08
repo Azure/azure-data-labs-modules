43913ef2a3c1436d6c87fa6825247bcd3b3cb223
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 11:19:04 +0100

    TF FMT

--- terraform/network-security-group/outputs.tf
+++ terraform/network-security-group/outputs.tf
@@ -1,20 +1,20 @@
 output "id" {
-	value =(
-		length(azurerm_network_security_group.adl_nsg) > 0 ?
-		azurerm_network_security_group.adl_nsg[0].id : ""
-	)
+  value = (
+    length(azurerm_network_security_group.adl_nsg) > 0 ?
+    azurerm_network_security_group.adl_nsg[0].id : ""
+  )
 }
 
 output "name" {
-	value =(
-		length(azurerm_network_security_group.adl_nsg) > 0 ?
-		azurerm_network_security_group.adl_nsg[0].name : ""
-	)
+  value = (
+    length(azurerm_network_security_group.adl_nsg) > 0 ?
+    azurerm_network_security_group.adl_nsg[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-	value =(
-		length(azurerm_network_security_group.adl_nsg) > 0 ?
-		azurerm_network_security_group.adl_nsg[0].resource_group_name : ""
-	)
+  value = (
+    length(azurerm_network_security_group.adl_nsg) > 0 ?
+    azurerm_network_security_group.adl_nsg[0].resource_group_name : ""
+  )
 }
