ed17a4650c133078947355fc0e42a09fd67029c1
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 11:37:14 +0100

    Make service-bus-namespace module optional

--- terraform/service-bus/service-bus-namespace/main.tf
+++ terraform/service-bus/service-bus-namespace/main.tf
@@ -1,48 +1,45 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/servicebus_namespace
 
 resource "azurerm_servicebus_namespace" "adl_sb" {
-  name                = "sb-${var.basename}"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  sku      = var.sku
-  capacity = var.capacity
-
+  name                          = "sb-${var.basename}"
+  location                      = var.location
+  resource_group_name           = var.rg_name
+  sku                           = var.sku
+  capacity                      = var.capacity
   public_network_access_enabled = var.is_sec_module ? false : true
+  tags                          = var.tags
 
-  tags = var.tags
+  count = var.module_enabled ? 1 : 0
 }
 
 resource "azurerm_servicebus_namespace_authorization_rule" "adl_sb_auth_rule" {
   name         = "sb-auth-rule"
-  namespace_id = azurerm_servicebus_namespace.adl_sb.id
+  namespace_id = azurerm_servicebus_namespace.adl_sb[0].id
+  listen       = var.listen
+  send         = var.send
+  manage       = var.manage
 
-  listen = var.listen
-  send   = var.send
-  manage = var.manage
+  count = var.module_enabled ? 1 : 0
 }
 
 # Private Endpoint configuration
 
 resource "azurerm_private_endpoint" "sb_pe" {
-  name                = "pe-${azurerm_servicebus_namespace.adl_sb.name}-namespace"
+  name                = "pe-${azurerm_servicebus_namespace.adl_sb[0].name}-namespace"
   location            = var.location
   resource_group_name = var.rg_name
   subnet_id           = var.subnet_id
-
   private_service_connection {
     name                           = "psc-sb-${var.basename}"
-    private_connection_resource_id = azurerm_servicebus_namespace.adl_sb.id
+    private_connection_resource_id = azurerm_servicebus_namespace.adl_sb[0].id
     subresource_names              = ["namespace"]
     is_manual_connection           = false
   }
-
   private_dns_zone_group {
     name                 = "private-dns-zone-group-kv"
     private_dns_zone_ids = var.private_dns_zone_ids
   }
-
-  count = var.is_sec_module ? 1 : 0
-
   tags = var.tags
+
+  count = var.module_enabled && var.is_sec_module ? 1 : 0
 }
