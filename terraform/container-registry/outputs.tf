eab9ab5b0cfb3dad6855521009d225c86f89c382
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 12:58:41 +0100

    Make container-registry module optional (#83)
    * Fix #75

--- terraform/container-registry/outputs.tf
+++ terraform/container-registry/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_container_registry.adl_cr.id
+  value = (
+    length(azurerm_container_registry.adl_cr) > 0 ?
+    azurerm_container_registry.adl_cr[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_container_registry.adl_cr.name
+  value = (
+    length(azurerm_container_registry.adl_cr) > 0 ?
+    azurerm_container_registry.adl_cr[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_container_registry.adl_cr.resource_group_name
+  value = (
+    length(azurerm_container_registry.adl_cr) > 0 ?
+    azurerm_container_registry.adl_cr[0].resource_group_name : ""
+  )
 }
