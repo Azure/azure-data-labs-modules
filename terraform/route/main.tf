2923aa762cfb2dfed41620151aebca580ba305a0
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 13:28:06 +0100

    Make route module optional (#125)

--- terraform/route/main.tf
+++ terraform/route/main.tf
@@ -3,8 +3,9 @@
 resource "azurerm_route" "adl_r" {
   name                = var.name
   resource_group_name = var.rg_name
+  route_table_name    = var.route_table_name
+  address_prefix      = var.address_prefix
+  next_hop_type       = var.next_hop_type
 
-  route_table_name = var.route_table_name
-  address_prefix   = var.address_prefix
-  next_hop_type    = var.next_hop_type
+  count = var.module_enabled ? 1 : 0
 }
--- terraform/route/outputs.tf
+++ terraform/route/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_route.adl_r.id
+  value = (
+    length(azurerm_route.adl_r) > 0 ?
+    azurerm_route.adl_r[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_route.adl_r.name
+  value = (
+    length(azurerm_route.adl_r) > 0 ?
+    azurerm_route.adl_r[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_route.adl_r.resource_group_name
+  value = (
+    length(azurerm_route.adl_r) > 0 ?
+    azurerm_route.adl_r[0].resource_group_name : ""
+  )
 }
