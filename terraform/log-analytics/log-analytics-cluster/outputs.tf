08ef6325b518a860a8e20d2e212e72c17a71c6f7
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 13:14:39 +0100

    Make log-analytics-cluster module optional (#105)

--- terraform/log-analytics/log-analytics-cluster/outputs.tf
+++ terraform/log-analytics/log-analytics-cluster/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_log_analytics_cluster.adl_logc.id
+  value = (
+    length(azurerm_log_analytics_cluster.adl_logc) > 0 ?
+    azurerm_log_analytics_cluster.adl_logc[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_log_analytics_cluster.adl_logc.name
+  value = (
+    length(azurerm_log_analytics_cluster.adl_logc) > 0 ?
+    azurerm_log_analytics_cluster.adl_logc[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_log_analytics_cluster.adl_logc.resource_group_name
+  value = (
+    length(azurerm_log_analytics_cluster.adl_logc) > 0 ?
+    azurerm_log_analytics_cluster.adl_logc[0].resource_group_name : ""
+  )
 }
