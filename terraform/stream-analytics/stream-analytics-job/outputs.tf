ea87661b3ba92c3f52728674389be1a53312a4b1
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 13:29:01 +0100

    Make stream-analytics-job module optional (#128)

--- terraform/stream-analytics/stream-analytics-job/outputs.tf
+++ terraform/stream-analytics/stream-analytics-job/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_stream_analytics_job.adl_asa.id
+  value = (
+    length(azurerm_stream_analytics_job.adl_asa) > 0 ?
+    azurerm_stream_analytics_job.adl_asa[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_stream_analytics_job.adl_asa.name
+  value = (
+    length(azurerm_stream_analytics_job.adl_asa) > 0 ?
+    azurerm_stream_analytics_job.adl_asa[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_stream_analytics_job.adl_asa.resource_group_name
+  value = (
+    length(azurerm_stream_analytics_job.adl_asa) > 0 ?
+    azurerm_stream_analytics_job.adl_asa[0].resource_group_name : ""
+  )
 }
