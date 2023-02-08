a5f142a1909b2e8d0ee7acf610203c87ab6bbcf2
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 09:47:41 +0100

    TF FMT files

--- terraform/application-insights/outputs.tf
+++ terraform/application-insights/outputs.tf
@@ -1,28 +1,28 @@
 output "id" {
-	value =(
-		length(azurerm_application_insights.adl_appi) > 0 ?
-		azurerm_application_insights.adl_appi[0].id : ""
-	)
+  value = (
+    length(azurerm_application_insights.adl_appi) > 0 ?
+    azurerm_application_insights.adl_appi[0].id : ""
+  )
 }
 
 output "instrumentation_key" {
-	value =(
-		length(azurerm_application_insights.adl_appi) > 0 ?
-		azurerm_application_insights.adl_appi[0].instrumentation_key : ""
-	)
-	sensitive = true
+  value = (
+    length(azurerm_application_insights.adl_appi) > 0 ?
+    azurerm_application_insights.adl_appi[0].instrumentation_key : ""
+  )
+  sensitive = true
 }
 
 output "name" {
-	value =(
-		length(azurerm_application_insights.adl_appi) > 0 ?
-		azurerm_application_insights.adl_appi[0].name : ""
-	)
+  value = (
+    length(azurerm_application_insights.adl_appi) > 0 ?
+    azurerm_application_insights.adl_appi[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-	value =(
-		length(azurerm_application_insights.adl_appi) > 0 ?
-		azurerm_application_insights.adl_appi[0].resource_group_name : ""
-	)
+  value = (
+    length(azurerm_application_insights.adl_appi) > 0 ?
+    azurerm_application_insights.adl_appi[0].resource_group_name : ""
+  )
 }
