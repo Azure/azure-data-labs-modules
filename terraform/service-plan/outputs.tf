f58da9220abf52c2590045aaf00dc422b6bc2f8b
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 13:20:05 +0100

    Make service-plan module optional (#115)

--- terraform/service-plan/outputs.tf
+++ terraform/service-plan/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_service_plan.adl_sp.id
+  value = (
+    length(azurerm_service_plan.adl_sp) > 0 ?
+    azurerm_service_plan.adl_sp[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_service_plan.adl_sp.name
+  value = (
+    length(azurerm_service_plan.adl_sp) > 0 ?
+    azurerm_service_plan.adl_sp[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_service_plan.adl_sp.resource_group_name
+  value = (
+    length(azurerm_service_plan.adl_sp) > 0 ?
+    azurerm_service_plan.adl_sp[0].resource_group_name : ""
+  )
 }
