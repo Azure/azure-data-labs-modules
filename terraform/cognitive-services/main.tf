b7dd66524c2d3b92055a0a80009abfc5e8b93556
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-08 12:54:22 +0100

    Make cognitive-services module optional (#82)
    * Make cognitive-services module optional
    
    * TF FMT
    
    * Set instance accessor

--- terraform/cognitive-services/main.tf
+++ terraform/cognitive-services/main.tf
@@ -5,17 +5,14 @@ data "http" "ip" {
 }
 
 resource "azurerm_cognitive_account" "adl_cog" {
-  name                = "cog-${var.basename}"
-  location            = var.location
-  resource_group_name = var.rg_name
-  kind                = var.kind
-
-  sku_name              = var.sku_name
-  custom_subdomain_name = "cog-${var.basename}"
-
+  name                               = "cog-${var.basename}"
+  location                           = var.location
+  resource_group_name                = var.rg_name
+  kind                               = var.kind
+  sku_name                           = var.sku_name
+  custom_subdomain_name              = "cog-${var.basename}"
   public_network_access_enabled      = var.is_sec_module ? false : true
   outbound_network_access_restricted = var.is_sec_module ? true : false
-
   # network_acls {
   #   default_action = "Deny"
   #   ip_rules       = []
@@ -23,31 +20,29 @@ resource "azurerm_cognitive_account" "adl_cog" {
 
   #   } */
   # }
-
   tags = var.tags
+
+  count = var.module_enabled ? 1 : 0
 }
 
 # Private Endpoint configuration
 
 resource "azurerm_private_endpoint" "cog_pe" {
-  name                = "pe-${azurerm_cognitive_account.adl_cog.name}-cog"
+  name                = "pe-${azurerm_cognitive_account.adl_cog[0].name}-cog"
   location            = var.location
   resource_group_name = var.rg_name
   subnet_id           = var.subnet_id
-
   private_service_connection {
     name                           = "psc-cog-${var.basename}"
-    private_connection_resource_id = azurerm_cognitive_account.adl_cog.id
+    private_connection_resource_id = azurerm_cognitive_account.adl_cog[0].id
     subresource_names              = ["account"]
     is_manual_connection           = false
   }
-
   private_dns_zone_group {
     name                 = "private-dns-zone-group-cog"
     private_dns_zone_ids = var.private_dns_zone_ids
   }
-
-  count = var.is_sec_module ? 1 : 0
-
   tags = var.tags
+
+  count = var.module_enabled && var.is_sec_module ? 1 : 0
 }
