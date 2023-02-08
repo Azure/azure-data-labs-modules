09c38c8de482c0d1d6b05f26c92ce081680ea51d
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 10:26:58 +0100

    Make container-registry module optional

--- terraform/container-registry/main.tf
+++ terraform/container-registry/main.tf
@@ -5,19 +5,18 @@ data "http" "ip" {
 }
 
 resource "azurerm_container_registry" "adl_cr" {
-  name                = "cr${var.basename}"
-  resource_group_name = var.rg_name
-  location            = var.location
-  sku                 = var.sku
-  admin_enabled       = var.admin_enabled
-
+  name                          = "cr${var.basename}"
+  resource_group_name           = var.rg_name
+  location                      = var.location
+  sku                           = var.sku
+  admin_enabled                 = var.admin_enabled
   public_network_access_enabled = var.is_sec_module ? false : true
-
   network_rule_set {
     default_action = var.firewall_default_action
   }
-
   tags = var.tags
+
+  count = var.module_enabled ? 1 : 0
 }
 
 # Private Endpoint configuration
@@ -27,20 +26,17 @@ resource "azurerm_private_endpoint" "cr_pe" {
   location            = var.location
   resource_group_name = var.rg_name
   subnet_id           = var.subnet_id
-
   private_service_connection {
     name                           = "psc-acr-${var.basename}"
     private_connection_resource_id = azurerm_container_registry.adl_cr.id
     subresource_names              = ["registry"]
     is_manual_connection           = false
   }
-
   private_dns_zone_group {
     name                 = "private-dns-zone-group-acr"
     private_dns_zone_ids = var.private_dns_zone_ids
   }
-
-  count = var.is_sec_module ? 1 : 0
-
   tags = var.tags
+
+  count = var.module_enabled && var.is_sec_module ? 1 : 0
 }
