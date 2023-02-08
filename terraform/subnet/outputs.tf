e6dadbd2cbd77403435aa007ddc10def30a2b949
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-08 12:50:57 +0100

    Make subnet module optional

--- terraform/subnet/outputs.tf
+++ terraform/subnet/outputs.tf
@@ -1,11 +1,20 @@
 output "id" {
-  value = azurerm_subnet.adl_snet.id
+  value = (
+    length(azurerm_subnet.adl_snet) > 0 ?
+    azurerm_subnet.adl_snet[0].id : ""
+  )
 }
 
 output "name" {
-  value = azurerm_subnet.adl_snet.name
+  value = (
+    length(azurerm_subnet.adl_snet) > 0 ?
+    azurerm_subnet.adl_snet[0].name : ""
+  )
 }
 
 output "resource_group_name" {
-  value = azurerm_subnet.adl_snet.resource_group_name
+  value = (
+    length(azurerm_subnet.adl_snet) > 0 ?
+    azurerm_subnet.adl_snet[0].resource_group_name : ""
+  )
 }
