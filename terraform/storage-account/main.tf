85d6c11a807c00d9436e74f854d5f5f547efc81a
Jared Rewerts <jarewert@microsoft.com>
2023-02-14 15:14:13 -0800

    Added support for SystemAssigned identity

--- terraform/storage-account/main.tf
+++ terraform/storage-account/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_storage_account" "adl_st" {
 resource "azurerm_role_assignment" "st_role_admin_c" {
   scope                = azurerm_storage_account.adl_st[0].id
   role_definition_name = "Contributor"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = data.azurerm_client_config.current.object_id == "" ? var.deployment_object_id : data.azurerm_client_config.current.object_id
 
   count = var.module_enabled ? 1 : 0
 }
@@ -41,7 +41,7 @@ resource "azurerm_role_assignment" "st_role_admin_c" {
 resource "azurerm_role_assignment" "st_role_admin_sbdc" {
   scope                = azurerm_storage_account.adl_st[0].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = data.azurerm_client_config.current.object_id == "" ? var.deployment_object_id : data.azurerm_client_config.current.object_id
 
   count = var.module_enabled ? 1 : 0
 }
