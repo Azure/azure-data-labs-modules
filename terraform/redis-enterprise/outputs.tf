3fb6cf17440feaabea83571fbc4bdffd7f6ed919
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-14 12:57:15 +0100

    Add description to outputs

--- terraform/redis-enterprise/outputs.tf
+++ terraform/redis-enterprise/outputs.tf
@@ -3,6 +3,7 @@ output "id" {
     length(azurerm_redis_enterprise_cluster.adl_redis_enterprise) > 0 ?
     azurerm_redis_enterprise_cluster.adl_redis_enterprise[0].id : ""
   )
+  description = "Resource identifier of the instance of Redis Enterprise cluster."
 }
 
 output "name" {
@@ -10,6 +11,7 @@ output "name" {
     length(azurerm_redis_enterprise_cluster.adl_redis_enterprise) > 0 ?
     azurerm_redis_enterprise_cluster.adl_redis_enterprise[0].name : ""
   )
+  description = "The name of the Redis Enterprise cluster."
 }
 
 output "hostname" {
@@ -17,6 +19,7 @@ output "hostname" {
     length(azurerm_redis_enterprise_cluster.adl_redis_enterprise) > 0 ?
     azurerm_redis_enterprise_cluster.adl_redis_enterprise[0].hostname : ""
   )
+  description = "The FQDN name of the host for the Redis Enterprise cluster."
 }
 
 output "resource_group_name" {
@@ -24,10 +27,12 @@ output "resource_group_name" {
     length(azurerm_redis_enterprise_cluster.adl_redis_enterprise) > 0 ?
     azurerm_redis_enterprise_cluster.adl_redis_enterprise[0].resource_group_name : ""
   )
+  description = "Resource Group where the Redis Enterprise cluster exists."
 }
 
 output "port" {
   value = 10000
+  description = "Port number on which the default database is listening on."
 }
 
 output "primary_access_key" {
@@ -35,5 +40,6 @@ output "primary_access_key" {
     length(azurerm_redis_enterprise_database.adl_redis_database) > 0 ?
     azurerm_redis_enterprise_database.adl_redis_database[0].primary_access_key : ""
   )
+  description = "Primary access key for the default database."
   sensitive = true
 }
