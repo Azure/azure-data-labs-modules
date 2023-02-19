13d46748c623fb50772a856ab10891bb763c8221
nachoalonsoportillo <ignacio.alonso@microsoft.com>
2023-02-19 11:35:42 +0100

    Fix dexcription of network_profile variable for aks-cluster

--- terraform/aks-cluster/variables.tf
+++ terraform/aks-cluster/variables.tf
@@ -53,18 +53,18 @@ variable "network_profile" {
   type        = map(string)
   description = <<EOT
     "   network_plugin - Network plugin to use for networking. Currently supported values are azure, kubenet and none. Changing this forces a new resource to be created.
-        network_mode  - Network mode to be used with Azure CNI. Possible values are bridge and transparent. Changing this forces a new resource to be created
-        network_policy -  Sets up network policy to be used with Azure CNI. Currently supported values are calico and azure.
-        dns_service_ip  - IP address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns).Changing this forces a new resource to be created.
+        network_mode - Network mode to be used with Azure CNI. Possible values are bridge and transparent. Changing this forces a new resource to be created
+        network_policy - Sets up network policy to be used with Azure CNI. Currently supported values are calico and azure.
+        dns_service_ip - IP address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns).Changing this forces a new resource to be created.
         docker_bridge_cidr - IP address (in CIDR notation) used as the Docker bridge IP address on nodes. Changing this forces a new resource to be created.
         outbound_type - The outbound (egress) routing method which should be used for this Kubernetes Cluster.Possible values are loadBalancer, userDefinedRouting, managedNATGateway and userAssignedNATGateway.
         pod_cidr - The CIDR to use for pod IP addresses. This field can only be set when network_plugin is set to kubenet. Changing this forces a new resource to be created.
         pod_cidrs - A list of CIDRs to use for pod IP addresses
         service_cidrs - A list of CIDRs to use for Kubernetes services. For single-stack networking a single IPv4 CIDR is expected.
-        ip_versions = Specifies a list of IP versions the Kubernetes Cluster will use to assign IP addresses to its nodes and pods. Possible values are IPv4 and/or IPv6. IPv4 must always be specified.
-        load_balancer_sku =  Specifies the SKU of the Load Balancer used for this Kubernetes Cluster. Possible values are basic and standard. Defaults to standard.
-        load_balancer_profile  = A load_balancer_profile block. This can only be specified when load_balancer_sku is set to standard.
-        nat_gateway_profile = - A nat_gateway_profile block. This can only be specified when load_balancer_sku is set to standard and outbound_type is set to managedNATGateway or userAssignedNATGateway.
+        ip_versions - Specifies a list of IP versions the Kubernetes Cluster will use to assign IP addresses to its nodes and pods. Possible values are IPv4 and/or IPv6. IPv4 must always be specified.
+        load_balancer_sku - Specifies the SKU of the Load Balancer used for this Kubernetes Cluster. Possible values are basic and standard. Defaults to standard.
+        load_balancer_profile - A load_balancer_profile block. This can only be specified when load_balancer_sku is set to standard.
+        nat_gateway_profile - A nat_gateway_profile block. This can only be specified when load_balancer_sku is set to standard and outbound_type is set to managedNATGateway or userAssignedNATGateway.
   EOT
   default     = {}
 }
