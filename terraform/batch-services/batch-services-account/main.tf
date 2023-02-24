af93517984cfd641e544159cf457f853e71c1c47
nachoalonsoportillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2023-02-24 21:20:19 +0100

    Update main.tf

--- terraform/batch-services/batch-services-account/main.tf
+++ terraform/batch-services/batch-services-account/main.tf
@@ -1,25 +1,21 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/batch_account
 
 resource "azurerm_batch_account" "adl_batch_account" {
-  name                                = "batch-account-${var.basename}"
+  name                                = "batchaccount${var.basename}"
   location                            = var.location
   resource_group_name                 = var.rg_name
   pool_allocation_mode                = var.pool_allocation_mode
   storage_account_id                  = var.storage_account_id
   storage_account_authentication_mode = "StorageKeys"
   allowed_authentication_modes        = var.allowed_authentication_modes
-
   identity {
     type = "SystemAssigned"
   }
-
   # key_vault_reference { # Required if var.pool_allocation is UserSubscription
   #   id  = ""
   #   url = ""
   # }
-
   public_network_access_enabled = var.public_network_access_enabled
-
   tags = var.tags
 
   count = var.module_enabled ? 1 : 0
@@ -42,5 +38,5 @@ resource "azurerm_private_endpoint" "batchacc_pe" {
   }
   tags = var.tags
 
-  count = var.module_enabled && (var.public_network_access_enabled ? false : true) ? 1 : 0
+  count = var.module_enabled && var.public_network_access_enabled ? 1 : 0
 }
