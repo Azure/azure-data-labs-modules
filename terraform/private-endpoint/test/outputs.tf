2b1e7f4eedd42b6e99f79f882ff09afdd56fee45
murggu <amurguzur@gmail.com>
2023-03-15 09:49:54 +0100

    fix #203

---
+++ terraform/private-endpoint/test/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = module.private_endpoint.id
+}
+
+output "name" {
+  value = module.private_endpoint.name
+}
+
+output "resource_group_name" {
+  value = module.private_endpoint.resource_group_name
+}
---
+++ terraform/private-endpoint/test/private_endpoint.tf
@@ -0,0 +1,52 @@
+module "private_endpoint" {
+  source                         = "../"
+  basename                       = "kv-${random_string.postfix.result}"
+  rg_name                        = module.local_rg.name
+  location                       = var.location
+  subnet_id                      = module.local_snet_default.id
+  private_connection_resource_id = module.local_key_vault.id
+  subresource_names              = ["vault"]
+  private_dns_zone_ids           = [module.local_pdnsz_kv.list[local.dns_kv_vault].id]
+  tags                           = {}
+}
+
+# Modules dependencies
+
+module "local_rg" {
+  source   = "../../resource-group"
+  basename = random_string.postfix.result
+  location = var.location
+  tags     = local.tags
+}
+
+module "local_vnet" {
+  source        = "../../virtual-network"
+  rg_name       = module.local_rg.name
+  basename      = random_string.postfix.result
+  location      = var.location
+  address_space = ["10.0.0.0/16"]
+}
+
+module "local_snet_default" {
+  source           = "../../subnet"
+  rg_name          = module.local_rg.name
+  name             = "vnet-${random_string.postfix.result}-kv-default"
+  vnet_name        = module.local_vnet.name
+  address_prefixes = ["10.0.6.0/24"]
+}
+
+module "local_key_vault" {
+  source        = "../../key-vault"
+  basename      = random_string.postfix.result
+  rg_name       = module.local_rg.name
+  location      = var.location
+  sku_name      = "standard"
+  is_sec_module = false
+}
+
+module "local_pdnsz_kv" {
+  source    = "../../private-dns-zone"
+  rg_name   = module.local_rg.name
+  dns_zones = [local.dns_kv_vault]
+  vnet_id   = module.local_vnet.id
+}
