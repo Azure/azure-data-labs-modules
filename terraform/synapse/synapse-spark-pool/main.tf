fef629bcb37645c4ae0068a1dd258a1a3e0182ef
murggu <amurguzur@gmail.com>
2023-03-17 11:09:37 +0100

    fix #202

--- terraform/synapse/synapse-spark-pool/main.tf
+++ terraform/synapse/synapse-spark-pool/main.tf
@@ -5,6 +5,10 @@ resource "azurerm_synapse_spark_pool" "syn_synsp" {
   synapse_workspace_id = var.synapse_workspace_id
   node_size_family     = var.node_size_family
   node_size            = var.node_size
+  cache_size           = var.cache_size
+  spark_version        = var.spark_version
+
+
   auto_scale {
     max_node_count = 50
     min_node_count = 3
--- terraform/synapse/synapse-spark-pool/variables.tf
+++ terraform/synapse/synapse-spark-pool/variables.tf
@@ -37,3 +37,19 @@ variable "module_enabled" {
   description = "Variable to enable or disable the module."
   default     = true
 }
+
+variable "cache_size" {
+  type        = number
+  description = "(Optional) The cache size in the Spark Pool."
+  default     = 100
+}
+
+variable "spark_version" {
+  type        = number
+  description = "(Optional) The Apache Spark version. Possible values are 2.4 , 3.1 , 3.2 and 3.3. Defaults to 2.4."
+  validation {
+    condition     = contains([2.4, 3.1, 3.2, 3.3], var.spark_version)
+    error_message = "Valid values for sku_name are \"Small\", \"Medium\", \"Large\", \"None\", \"XLarge\", \"XXLarge\", or \"XXXLarge\"."
+  }
+  default = 2.4
+}
