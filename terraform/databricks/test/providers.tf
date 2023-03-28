5bb5e5e38db649b6e5b7488b44b8023241f50bb5
Aitor Murguzur <amurguzur@gmail.com>
2023-03-28 09:43:20 +0200

    upgrade to 3.49.0 (#219)
    * upgrade to 3.49.0

--- terraform/databricks/test/providers.tf
+++ terraform/databricks/test/providers.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.47.0"
+      version = "= 3.49.0"
     }
     databricks = {
       source  = "databricks/databricks"
