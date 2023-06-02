b2226aa1fe89af08d7f775faefa4e4025edee286
murggu <amurguzur@gmail.com>
2023-06-02 12:27:30 +0200

    cosmetic changes

--- terraform/fabric/fabric-capacity/test/providers.tf
+++ terraform/fabric/fabric-capacity/test/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-adl-terraform-state"
-    storage_account_name = "stadlterraformstate"
-    container_name       = "default"
-    key                  = "fabriccapacity.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-adl-terraform-state"
+  #   storage_account_name = "stadlterraformstate"
+  #   container_name       = "default"
+  #   key                  = "fabriccapacity.terraform.tfstate"
+  # }
 
   required_providers {
     azurerm = {
