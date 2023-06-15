ad41ddd289a5579fd218a66060590ba904c22672
murggu <amurguzur@gmail.com>
2023-06-15 10:07:58 +0200

    upgrade to 3.61.0

--- terraform/databricks/databricks-workspace/test/providers.tf
+++ terraform/databricks/databricks-workspace/test/providers.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.56.0"
+      version = "= 3.61.0"
     }
     databricks = {
       source  = "databricks/databricks"
