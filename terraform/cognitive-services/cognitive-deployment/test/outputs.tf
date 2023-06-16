4c4b9e58dfeb72f129099c7046889635e345e4ca
Jorge Docampo <jorge.docampo@outlook.com>
2023-06-16 15:57:07 +0000

    fix output

--- terraform/cognitive-services/cognitive-deployment/test/outputs.tf
+++ terraform/cognitive-services/cognitive-deployment/test/outputs.tf
@@ -1,11 +1,11 @@
 output "id" {
-  value = module.cognitive_services.id
+  value = module.cognitive_deployment.id
 }
 
 output "name" {
-  value = module.cognitive_services.name
+  value = module.cognitive_deployment.name
 }
 
 output "resource_group_name" {
-  value = module.cognitive_services.resource_group_name
+  value = module.cognitive_deployment.resource_group_name
 }
